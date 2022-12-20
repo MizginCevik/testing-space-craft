8d696100b965e40d4bcdaf1f9d2510c7cd199a8e
mizgin <mizgincvk@gmail.com>
2022-12-20 13:36:10 -0500

    unit test for CreatePlayerServiceImplTest

---
+++ src/test/java/com/cydeo/service/impl/CreatePlayerServiceImplTest.java
@@ -0,0 +1,88 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.CreateGameDTO;
+import com.cydeo.entity.Player;
+import com.cydeo.enumtype.Boost;
+import com.cydeo.enumtype.Level;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CreatePlayerServiceImplTest {
+
+    private CreatePlayerServiceImpl createPlayerService;
+
+    @BeforeEach
+    public void setUp(){
+        createPlayerService = new CreatePlayerServiceImpl();
+    }
+
+    @Test
+    public void should_create_player_with_big_bomb_boost_type_and_level_easy(){
+        // given
+        CreateGameDTO createGameDTO = new CreateGameDTO();
+        createGameDTO.setBoost(Boost.BIG_BOMB);
+        createGameDTO.setLevel(Level.EASY);
+        createGameDTO.setUsername("username");
+
+        //when
+        Player player = createPlayerService.createPlayer(createGameDTO);
+
+        //then
+        assertEquals(player.getHealth(), 2000);
+        assertEquals(player.getArmor(), 7);
+        assertEquals(player.getShootPower(), 5010);
+    }
+
+    @Test
+    public void should_create_player_with_extra_shield_boost_type_and_level_easy(){
+        // given
+        CreateGameDTO createGameDTO = new CreateGameDTO();
+        createGameDTO.setBoost(Boost.EXTRA_SHIELD);
+        createGameDTO.setLevel(Level.EASY);
+        createGameDTO.setUsername("username");
+
+        //when
+        Player player = createPlayerService.createPlayer(createGameDTO);
+
+        //then
+        assertEquals(player.getHealth(), 4145);
+        assertEquals(player.getArmor(), 242);
+        assertEquals(player.getShootPower(), 10);
+    }
+
+    @Test
+    public void should_create_player_with_super_ammo_boost_type_and_level_easy(){
+        // given
+        CreateGameDTO createGameDTO = new CreateGameDTO();
+        createGameDTO.setBoost(Boost.SUPER_AMMO);
+        createGameDTO.setLevel(Level.EASY);
+        createGameDTO.setUsername("username");
+
+        //when
+        Player player = createPlayerService.createPlayer(createGameDTO);
+
+        //then
+        assertEquals(player.getHealth(), 2000);
+        assertEquals(player.getArmor(), 7);
+        assertEquals(player.getShootPower(), 160);
+    }
+
+    @Test
+    public void should_throw_exception_when_boost_type_is_high_speed(){
+        // given
+        CreateGameDTO createGameDTO = new CreateGameDTO();
+        createGameDTO.setBoost(Boost.HIGH_SPEED);
+        createGameDTO.setLevel(Level.EASY);
+        createGameDTO.setUsername("username");
+
+        //then
+        RuntimeException runtimeException = assertThrows(RuntimeException.class, () ->{
+            createPlayerService.createPlayer(createGameDTO);
+        });
+
+        assertEquals(runtimeException.getMessage(),"Boost type must be valid");
+    }
+
+}
